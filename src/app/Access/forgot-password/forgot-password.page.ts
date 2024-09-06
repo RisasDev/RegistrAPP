0911a2b2558acb544b930ad40827224e9fd61f2f
cetecom_avaras <mauriciouch15@gmail.com>
2024-09-06 12:48:15 -0400

    Pagina de registro + calendario

--- src/app/Access/forgot-password/forgot-password.page.ts
+++ src/app/Access/forgot-password/forgot-password.page.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ForgotPasswordPage implements OnInit {
 
    /* Objeto JSON para usuario */
-   user = {
+  user = {
     username: ''
   };
 
--- src/app/Access/register/register.page.ts
+++ src/app/Access/register/register.page.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
+  user = {
+    username: '',
+    name: '',
+    lastname: '',
+    birthdate: '',
+    password: '',
+    confirmPassword: ''
+  };
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  showPassword = false;
+  showConfirmPassword = false;
+  passwordtoggleicon = 'eye'
+
+  togglePassword(): void {
+    this.showPassword = !this.showPassword;
+
+    if (this.passwordtoggleicon == 'eye') {
+      this.passwordtoggleicon = 'eye-off'
+    } 
+    else {
+      this.passwordtoggleicon = 'eye'
+    }
+  }
+
+  toggleConfirmPassword(): void {
+    this.showConfirmPassword = !this.showConfirmPassword;
+
+    if (this.passwordtoggleicon == 'eye') {
+      this.passwordtoggleicon = 'eye-off'
+    } 
+    else {
+      this.passwordtoggleicon = 'eye'
+    }
+  }
 }
